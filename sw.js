19dfcb75c5ba97c0d0dc416732a2e6dae085a2c8
zhouyuansheng <634959945@qq.com>
2026-07-26 10:50:09 +0800

    PWA service worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Simple cache-first service worker for offline support
+const CACHE = 'mr-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.json'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
